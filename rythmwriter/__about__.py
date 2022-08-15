d661f85edfca271993aea739a3e3846d9e95b3db
drgnfireyellow <ethan@milbert.dev>
2022-08-15 15:50:17 -0400

    First release

--- rythmwriter/__about__.py
+++ rythmwriter/__about__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022-present U.N. Owen <void@some.where>
+# SPDX-FileCopyrightText: 2022-present U.N. DrgnFireYellow <ethan@milbert.dev>
 #
-# SPDX-License-Identifier: MIT
-__version__ = '0.0.1'
+# SPDX-License-Identifier: Apache-2.0
+__version__ = '0.1.0'
--- rythmwriter/__init__.py
+++ rythmwriter/__init__.py
@@ -1,3 +1,28 @@
-# SPDX-FileCopyrightText: 2022-present U.N. Owen <void@some.where>
+# SPDX-FileCopyrightText: 2022-present U.N. DrgnFireYellow <ethan@milbert.dev>
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: Apache-2.0
+import simpleaudio as _sa
+from time import sleep as _sleep
+
+def _playsound(sound):
+    audio = _sa.WaveObject.from_wave_file(sound)
+    audio.play()
+
+class Rythm:
+    def __init__(self, beats: list) -> None:
+        self.beats = beats
+    def play(self) -> None:
+        for i in self.beats:
+            if i == "quarter":
+                _playsound("assets/drum.wav")
+                _sleep(1)
+            elif i == "eighth":
+                _playsound("assets/drum.wav")
+                _sleep(0.5)
+                _playsound("assets/drum.wav")
+                _sleep(0.5)
+            elif i == "rest":
+                _sleep(1)
+
+if __name__ == "__main__":
+    Rythm(["quarter", "eighth", "rest", "quarter", "eighth"]).play()
